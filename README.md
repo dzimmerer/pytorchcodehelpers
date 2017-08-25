10dbed0168d506fcf39fe6a7c27f8d7a86d22aae
david <d.zimmerer@gmail.com>
2017-08-25 22:41:05 +0200

    First commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pytorch Code Helpers
+
+Just some small helpers while coding pytorch. 
+
+They basically allow a very rough inspection into your current models/ code. 
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pytorchcodehelpers',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='Apache License Version 2.0',
+    author='David',
+    author_email='d.zimmerer@gmail.com',
+    description='Just some fun / code inspection with pytorch',
+    install_requires=['numpy', 'torch'],
+)
